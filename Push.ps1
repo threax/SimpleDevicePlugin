a1fabf3be087880d3060e8e6fa1bfcee5709bf1f
Andrew Piper <andrew@threax.com>
2026-03-02 15:36:18 -0500

    Add push script.

---
+++ Push.ps1
@@ -0,0 +1,8 @@
+git push
+git push --tags
+git push https://github.com/threax/SimpleDevicePlugin.git
+git push https://github.com/threax/SimpleDevicePlugin.git --tags
+git push https://gitlab.com/threax-software/SimpleDevicePlugin.git
+git push https://gitlab.com/threax-software/SimpleDevicePlugin.git --tags
+git push https://threax@dev.azure.com/threax/ThreaxSoftware/_git/SimpleDevicePlugin
+git push https://threax@dev.azure.com/threax/ThreaxSoftware/_git/SimpleDevicePlugin --tags
